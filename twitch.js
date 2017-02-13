18e256ffb30bc9170d9fcfc1be363f119058ce91
san00 <c5d59d07@opayq.com>
2017-02-13 21:44:49 +0000

    Start refactoring JS
    Got rid of some variables as no longer needed to check data returned
    from api.

--- twitch.js
+++ twitch.js
@@ -1,60 +1,65 @@
-$(document).ready(function() {
-    var users = ["ESL_SC2", "freecodecamp", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+   $(document).ready(function() {
+       var users = ["ESL_SC2", "freecodecamp", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-    var userName = [];
-    var userLogo = [];
-    var userStatus = [];
-    var userURL = [];
-    var userOnline = [];
-    var message = [];
+      //  var userName = [];
+       var userLogo;
+      //  var userStatus = [];
+       var userURL = [];
+       var userOnline;
+      //  var message = [];
 
 
-    for (var i = 0; i < users.length; i++) {
-        var apiResult = "https://wind-bow.gomix.me/twitch-api/channels/" + users[i];
+       for (var i = 0; i < users.length; i++) {
+           var apiResult = "https://wind-bow.gomix.me/twitch-api/channels/" + users[i];
 
-        streaming(i);
-    };
+           streaming(i);
+       };
 
-    function streaming() {
+       function streaming() {
 
-        var apiStream = "https://wind-bow.gomix.me/twitch-api/streams/" + users[i];
-        var apiUser = "https://wind-bow.gomix.me/twitch-api/users/" + users[i];
+           var apiStream = "https://wind-bow.gomix.me/twitch-api/streams/" + users[i];
 
-        $.ajax({
-            url: apiResult,
-            dataType: "jsonp",
-            success: function(info) {
+           $.ajax({
+               url: apiResult,
+               dataType: "jsonp",
+               success: function(info) {
 
-                userName[i] = info.display_name;
-                userLogo[i] = ("<img src=" + info.logo + ">")
-                userStatus[i] = info.status;
-                userURL[i] = info.url;
+                  //  userName[i] =
+                   info.display_name; // user names
+                  //  userLogo[i] =
+                   ("<img src=" + info.logo + ">") //user logo's
+                  //  userStatus[i] =
+                   info.status;
+                  //  userURL[i] =
+                   info.url;
 
-                $.ajax({
-                    url: apiStream,
-                    dataType: "jsonp",
-                    success: function(data) {
+                   $.ajax({
+                       url: apiStream,
+                       dataType: "jsonp",
+                       success: function(data) {
 
-                        userName[i] = info.display_name;
-                        userLogo[i] = ("<img src=" + info.logo + ">")
-                        userStatus[i] = info.status;
-                        userURL[i] = info.url;
+                          //  userName[i] =
+                           info.display_name;
+                           userLogo = ("<img src=" + info.logo + ">")
 
-                        if (data.stream === null)
-                            userOnline[i] = "Offline";
-                        else
-                            userOnline[i] = "Online";
+                          //  userStatus[i] =
+                           info.status;
+                           userURL = info.url;
 
-                        $("#status").append(
+                           if (data.stream === null)
+                               userOnline = "Offline";
+                            else
+                               userOnline = "Online";
+
+                           $("#status").append(
                             //  userLogo[i] +
-                            "<a href =" + info.url + " " + ">" + userLogo +
-                            "<br></br>" + info.display_name + "</a>" + "<br></br>" + " "
-                            + " " + info.status + " " + "<br></br>" + "Currently"
-                            + userOnline + " " + "<br></br>" +
-                            "_________________________________" + "<br></br>");
-                    }
-                });
-            }
-        })
-    };
-});
+                              " " + "<a href =" + info.url+ " " + ">" + userLogo + "<br></br>"
+                           + info.display_name + "</a>" + "<br></br>" + " " + " "+info.status + " "
+                           +  "<br></br>" + " Currently"+ " "+userOnline + " " + "<br></br>"
+                         + "_________________________________" + "<br></br>");
+                       }
+                   });
+               }
+           })
+       };
+   });
